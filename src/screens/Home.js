e2dee8f149cff1ce10ad2718e5ad3225fe81aace
Ryder Xie <jelinenaro@gmail.com>
2018-09-28 16:02:44 +0800

    use css grid for layout

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import Grid from '@material-ui/core/Grid';
 
 import ImageCard from '../components/ImageCard';
 import DictumEdit from '../components/DictumEdit';
 
 const Container = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 24px;
+  align-items: start;
   max-width: 1200px;
+  min-width: 300px;
   margin: auto;
   padding-top: 24px;
 `;
@@ -15,14 +19,8 @@ export default class Home extends Component {
   render() {
     return (
       <Container>
-        <Grid container spacing={24}>
-          <Grid item xs={12} md>
-            <ImageCard />
-          </Grid>
-          <Grid item xs={12} md>
-            <DictumEdit />
-          </Grid>
-        </Grid>
+        <ImageCard />
+        <DictumEdit />
       </Container>
     );
   }
